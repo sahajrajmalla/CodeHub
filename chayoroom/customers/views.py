caf60ecce589d8686d9d90f80d7cb26e70cae692
Nabin <nabin@localhost.localdomain>
2019-06-27 18:09:12 +0545

    database connected with HTML to sahaj project also

--- chayoroom/customers/views.py
+++ chayoroom/customers/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import customer
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World!, I'm in the project bro !!")
+    userlist = customer.objects.all()
+    return render(request, 'index.html',
+                  {'persons': userlist})
 
 
 def vip(request):
