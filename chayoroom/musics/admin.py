de8fc67f2b35c0f7d7bbc593ae7db9694b7bdf4c
Nabin <nabin@localhost.localdomain>
2019-06-27 17:31:22 +0545

    music list added

--- chayoroom/musics/admin.py
+++ chayoroom/musics/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import new_add
 
-admin.site.register(new_add)
+
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genere')
+
+
+admin.site.register(new_add, MusicAdmin)
